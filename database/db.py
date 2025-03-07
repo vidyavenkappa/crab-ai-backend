60189c4dc89e3d6da4ac8fb932bf1de50059c257
vidyavenkappa <vidyavenkappa@gmail.com>
2025-03-06 17:24:56 -0800

    made changes for the error

--- database/db.py
+++ database/db.py
@@ -1,14 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
 # SQLite database URL
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in environment variables!")
+
+# Ensure the correct format for PostgreSQL
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+
 
 # Create SQLAlchemy engine
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
+    DATABASE_URL, 
     connect_args={"check_same_thread": False}
 )
 
