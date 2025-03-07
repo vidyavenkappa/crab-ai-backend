8b9f1155005893522dbe95b04f2c3a5745aee529
vidyavenkappa <vidyavenkappa@gmail.com>
2025-03-06 19:35:20 -0800

    changed the port

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ def health_check():
     return {"status": "healthy"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
